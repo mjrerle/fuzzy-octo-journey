cd16ab7b869e630a99ff644fb90ca2fb12206fd2
mjrerle <matterle@live.com>
2017-09-03 17:22:45 -0600

    closes #30
    Signed-off-by: mjrerle <matterle@live.com>

--- src/main/java/edu/csu2017fa314/T29/View/View.java
+++ src/main/java/edu/csu2017fa314/T29/View/View.java
@@ -1,16 +1,72 @@
 package edu.csu2017fa314.T29.View;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class View
 {
    private int totalDistance;
 
-   public void setTotalDistance(int distance) 
-   {
-       totalDistance = distance; 
+   private JSONArray list;
+
+   public View(){
+      list = new JSONArray();
+      totalDistance =0;
+   }
+   @SuppressWarnings("unchecked")
+   void readFile(String file) throws IOException{
+
+      try{
+         JSONObject obj = new JSONObject();
+         Scanner in = new Scanner(new File(file));
+         String line;
+         while(in.hasNext()){
+            line = in.nextLine();
+            String[] array = line.split(",");
+            obj.put("start", array[0]);
+            obj.put("end", array[1]);
+            obj.put("distance", array[2]);
+            list.add(obj);
+            totalDistance+= Double.parseDouble(array[2]);
+         }
+         in.close();
+      }
+      catch(IOException e){
+         e.printStackTrace();
+      }
    }
 
-   public int getTotalDistance() 
+   @SuppressWarnings("unchecked")
+   void writeFile() throws IOException{
+      try{
+         PrintWriter writer = new PrintWriter("C:\\Users\\matte\\workspace\\cs314\\T29\\data\\out\\itinerary.json");
+         writer.print(list.toString());
+         writer.close();
+      }
+      catch (IOException e){
+         e.printStackTrace();
+      }
+   }
+
+   JSONArray getList(){
+      return list;
+   }
+   void setTotalDistance(int distance)
+   {
+      totalDistance = distance;
+   }
+
+   int getTotalDistance()
    {
       return totalDistance;
    }
+   void printList(){
+       System.out.print(list.toString());
+   }
+
 
 }
--- src/test/java/edu/csu2017fa314/T29/View/TestView.java
+++ src/test/java/edu/csu2017fa314/T29/View/TestView.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+
 
 public class TestView
 {
@@ -13,6 +15,7 @@ public class TestView
     public void setUp() throws Exception 
     {
         v = new View();
+        assertTrue(v.getTotalDistance()==0);
     }
 
     @Test 
@@ -24,4 +27,23 @@ public class TestView
         assertTrue(v.getTotalDistance() == -4);
     }
 
+    @Test
+    public void testReadFile() throws IOException{
+        v.readFile("C:\\Users\\matte\\workspace\\cs314\\T29\\src\\test\\java\\edu\\csu2017fa314\\T29\\View\\test.csv");
+        assertTrue(v.getList()!=null);
+    }
+
+    @Test
+    public void testWriteFile() throws IOException{
+        assertTrue(v.getList()!=null);
+        v.readFile("C:\\Users\\matte\\workspace\\cs314\\T29\\data\\test\\test.csv");
+        v.writeFile();
+    }
+
+    @Test
+    public void testTotalDistance() throws IOException{
+        v.readFile("C:\\Users\\matte\\workspace\\cs314\\T29\\data\\test\\test.csv");
+        v.writeFile();
+        System.out.println(v.getTotalDistance());
+    }
 }
