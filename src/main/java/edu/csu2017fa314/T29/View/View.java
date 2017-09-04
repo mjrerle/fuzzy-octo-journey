71c9e99ac9508535a1cd853d45d786f6f702cb2a
mjrerle <matterle@live.com>
2017-09-03 19:49:31 -0600

    closes #35
    Signed-off-by: mjrerle <matterle@live.com>

--- src/main/java/edu/csu2017fa314/T29/View/View.java
+++ src/main/java/edu/csu2017fa314/T29/View/View.java
@@ -16,67 +16,99 @@ public class View
    private int totalDistance;
 
    private JSONArray list;
+   //provided by the JSON-simple library
 
    public View(){
       list = new JSONArray();
+      //init list
       totalDistance =0;
+      //housekeeping
    }
    @SuppressWarnings("unchecked")
-   void readFile(String file) throws IOException{
+   void readFile(String file){
 
       try{
 
          Map map = new LinkedHashMap<String,String>();
+         //map for keeping json object ordered
          Scanner in = new Scanner(new File(file));
          String line;
          while(in.hasNext()){
+             //expects a csv in the format <start>,<end>,<distance>
             line = in.nextLine();
             String[] array = line.split(",");
+            //create array out of the line
             if(array.length!=3){
-                System.out.println("readFile detected bad input data");
-                System.exit(1);
+                //checks line format
+                throw new Exception();
             }
             map.put("start", array[0]);
             map.put("end", array[1]);
             map.put("distance", array[2]);
+            //add elements in ordered fashion
             list.add(new JSONObject(map));
+            //the first element will look like [{"key":"value"}], thankfully the library will handle the heavy lifting
+            //make a new JSONObject with the given map and put that into the JSONArray
             totalDistance+= Double.parseDouble(array[2]);
+            //keep track of the distance
          }
          in.close();
+         //no leaks please
       }
       catch(IOException e){
          e.printStackTrace();
+      }
+      catch (Exception e) {
+          System.out.println("Line length is not equal to 3, this is bad data");
+          e.printStackTrace();
       }
    }
 
    @SuppressWarnings("unchecked")
    void writeFile() throws IOException{
+
       try{
+         if(list.isEmpty()){
+            System.out.println();
+            throw new Exception();
+         }
          PrintWriter writer = new PrintWriter("data/out/itinerary.json");
+         //writes to a specific filepath
          Gson gson = new GsonBuilder().setPrettyPrinting().create();
+         //gson allows for pretty printing (not available with just json-simple)
+          //the gson object will transform the json string or array
          writer.println(gson.toJson(list));
+         //write to the file
          writer.close();
+         //no leaks...
       }
       catch (IOException e){
          e.printStackTrace();
+      } catch (Exception e) {
+          System.out.println("Please read a non-empty file before calling writeFile");
+          e.printStackTrace();
       }
    }
 
    JSONArray getList(){
+       //helper method for debugging
       return list;
    }
    void setTotalDistance(int distance)
    {
+       //trivial
       totalDistance = distance;
    }
 
    int getTotalDistance()
    {
+       //trivial
       return totalDistance;
    }
-   void printList(){
-       System.out.println(list.toString());
-   }
+//   void printList(){
+//       //debugging method (deprecated)
+//       System.out.println(list.toString());
+//   }
 
 
 }
--- src/test/java/edu/csu2017fa314/T29/View/TestView.java
+++ src/test/java/edu/csu2017fa314/T29/View/TestView.java
@@ -10,7 +10,9 @@ import java.io.IOException;
 public class TestView
 {
     private View v;
-
+    public static void print(Object object){
+        System.out.println(object);
+    }
     @Before
     public void setUp() throws Exception 
     {
@@ -36,14 +38,48 @@ public class TestView
     @Test
     public void testWriteFile() throws IOException{
         assertTrue(v.getList()!=null);
-        v.readFile("data/test/test2.csv");
+        v.readFile("data/test/test.csv");
         v.writeFile();
+    }
+
+    @Test
+    public void testBadFile() throws IOException{
+        v.readFile("data/test/test2.csv");
+    }
+
+    @Test
+    public void testMisuse1() throws IOException{
+        v.writeFile();
+        v.readFile("data/test/test.csv");
+    }
+
+    @Test
+    public void testMisuse2() throws IOException{
+        v.readFile("data/test/evil.csv");
+        v.writeFile();
+    }
+
+    @Test
+    public void testOverflow() throws IOException{
+        v.setTotalDistance(999999999);
+        v.readFile("data/test/overflow.csv");
+        v.writeFile();
+        System.out.println(v.getTotalDistance());
+    }
+
+
+    @Test
+    public void testUnderflow() throws IOException{
+        v.setTotalDistance(-999999999);
+        v.readFile("data/test/underflow.csv");
+        v.writeFile();
+        System.out.println(v.getTotalDistance());
     }
 
     @Test
     public void testTotalDistance() throws IOException{
         v.readFile("data/test/test.csv");
         v.writeFile();
-        System.out.println(v.getTotalDistance());
+        print(v.getTotalDistance());
     }
 }
