46294db6e4631e5bab272fd3698fd20663a646bb
TimStroup <tstroup@rams.colostate.edu>
2017-09-06 18:18:22 -0600

    Addition of a few extras

--- web/src/components/Home/Pair/Pair.jsx
+++ web/src/components/Home/Pair/Pair.jsx
@@ -1,16 +1,20 @@
 import React, {Component} from 'react';
-
-let Pair = ({start, end, dist}) => <tbody
+//I added a new variable runningDist in order to output it
+let Pair = ({start, end, dist,runningDist}) => <tbody
     className="pair">
     <tr>
         <td>
-            <h5>{start}</h5>
+            <h4>{start}</h4>
         </td>
         <td>
-            <h5>{end}</h5>
+            <h4>{end}</h4>
         </td>
         <td>
-            <h5>{dist}</h5>
+            <h4>{dist}</h4>
+        </td>
+
+        <td>
+            <h4>{runningDist}</h4>
         </td>
     </tr>
 </tbody>;
