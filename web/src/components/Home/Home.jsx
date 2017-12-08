5dde41b95c38fb0e90f8a23771e444bd277b3432
Trey Yu <trey@Treys-MacBook-Pro.local>
2017-12-07 19:33:55 -0700

    Updating work

--- web/src/components/Home/Home.jsx
+++ web/src/components/Home/Home.jsx
@@ -6,6 +6,7 @@ import InlineSVG from "svg-inline-react";
 import Reorder from 'react-reorder';
 import reorderImmutable from 'react-reorder';
 import reorderFromToImmutable from 'react-reorder';
+import reorder from 'react-reorder'
 
 class Home extends React.Component {
     constructor(props) {
@@ -310,9 +311,25 @@ class Home extends React.Component {
                     <section id="customOrdering" style={{position: "relative", float: "right", marginRight: "10%"}}>
                         <h4><strong>Drag and Drop the order you would like!</strong></h4>
                         <Reorder
-                            reorderId="My-List"
-                            onReorder={this.onReorder.bind(this)}
-                            >
+                            reorderId="my-list" // Unique ID that is used internally to track this list (required)
+                            reorderGroup="reorder-group" // A group ID that allows items to be dragged between lists of the same group (optional)
+                            //getRef={this.storeRef.bind(this)} // Function that is passed a reference to the root node when mounted (optional)
+                            component="ul" // Tag name or Component to be used for the wrapping element (optional), defaults to 'div'
+                            placeholderClassName="placeholder" // Class name to be applied to placeholder elements (optional), defaults to 'placeholder'
+                            draggedClassName="dragged" // Class name to be applied to dragged elements (optional), defaults to 'dragged'
+                            lock="horizontal" // Lock the dragging direction (optional): vertical, horizontal (do not use with groups)
+                            holdTime={500} // Default hold time before dragging begins (mouse & touch) (optional), defaults to 0
+                            touchHoldTime={500} // Hold time before dragging begins on touch devices (optional), defaults to holdTime
+                            mouseHoldTime={200} // Hold time before dragging begins with mouse (optional), defaults to holdTime
+                            onReorder={this.onReorder.bind(this)} // Callback when an item is dropped (you will need this to update your state)
+                            autoScroll={true} // Enable auto-scrolling when the pointer is close to the edge of the Reorder component (optional), defaults to true
+                            disabled={false} // Disable reordering (optional), defaults to false
+                            disableContextMenus={true} // Disable context menus when holding on touch devices (optional), defaults to true
+                            placeholder={
+                                <div className="custom-placeholder" /> // Custom placeholder element (optional), defaults to clone of dragged element
+                            }
+
+                        >
                             {
                                 this.state.selectedLocations.map((item) => (
                                     <li>
@@ -589,26 +606,18 @@ class Home extends React.Component {
     }
 
     async onReorder(event, previousIndex, nextIndex, fromId, toId) {
-        if (fromId === toId) {
-            const list = reorderImmutable(this.state[fromId], previousIndex, nextIndex);
 
-            this.setState({
-                [fromId]: list
-            });
-        } else {
-            const lists = reorderFromToImmutable({
-                from: this.state[fromId],
-                to: this.state[toId]
-            }, previousIndex, nextIndex);
+        console.log("This is what being binded: " + event);
+        console.log("This is previousIndex: " + previousIndex);
+        console.log("This is nextIndex: " + nextIndex);
+        console.log("This is fromId: " + fromId);
+        console.log("This is toId: " + toId);
+        this.setState({
 
-            this.setState({
-                [fromId]: lists.from,
-                [toId]: lists.to
-            })
-        }
+            selectedLocations: reorder(this.state.selectedLocations, fromIndex, toIndex),
+
+        })
     }
-
-
 
     async handleOptimization(event) {
         let input = event.target.value;
